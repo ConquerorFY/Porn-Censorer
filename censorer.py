ea9db7b5462a409447ea3919a611c65c14e4e4e9
ConquerorFY <ryanrlfy33@gmail.com>
2024-06-17 12:30:18 +0800

    Added Window Focused Mode

--- censorer.py
+++ censorer.py
@@ -2,6 +2,10 @@ import cv2
 import numpy as np
 import mss
 import mss.tools
+import win32gui
+import win32ui
+from ctypes import windll
+import pygetwindow as gw
 from PyQt5.QtGui import (QPainter, QPen, QColor)
 from PyQt5.QtWidgets import (QMainWindow, QApplication)
 from PyQt5.QtCore import (Qt, pyqtSignal, QObject)
@@ -13,8 +17,9 @@ from utils import delete_rename_file
 
 SCREEN_IMAGE_PATH = "./inputs/input.png"
 CURRENT_SCREEN_IMAGE_PATH = "./inputs/current-input.png"
+CURRENT_SCREEN_BMP_PATH = "./inputs/current-input.bmp"
 CENSOR_SCREEN_IMAGE_PATH = "./inputs/input-censored.png"
-IMAGE_SIMILARITY = 80       # for now use similarity to check whether images are the same (before and after censored)
+IMAGE_SIMILARITY = 90      # for now use similarity to check whether images are the same (before and after censored)
 
 class Communicate(QObject):
     position = pyqtSignal(int, int, int, int)
@@ -68,15 +73,53 @@ comm = Communicate()
 censor_blocks = []
 
 def capture_save_screen():
-    with mss.mss() as sct:
-        monitor = sct.monitors[1]  # Capture the primary monitor
-        screenshot = sct.grab(monitor)
+    # with mss.mss() as sct:
+    #     monitor = sct.monitors[1]  # Capture the primary monitor
+    #     screenshot = sct.grab(monitor)
 
-        # Convert the screenshot to a numpy array
-        img = np.array(screenshot)
-        # Convert the image from BGRA to BGR (removing the alpha channel)
-        img_bgr = cv2.cvtColor(img, cv2.COLOR_BGRA2BGR)
-        cv2.imwrite(CURRENT_SCREEN_IMAGE_PATH, img_bgr)
+    #     # Convert the screenshot to a numpy array
+    #     img = np.array(screenshot)
+    #     # Convert the image from BGRA to BGR (removing the alpha channel)
+    #     img_bgr = cv2.cvtColor(img, cv2.COLOR_BGRA2BGR)
+    #     cv2.imwrite(CURRENT_SCREEN_IMAGE_PATH, img_bgr)
+
+    window_name = gw.getActiveWindowTitle()
+    # print(window_name)
+
+    try:
+        # Adapted from https://stackoverflow.com/questions/19695214/screenshot-of-inactive-window-printwindow-win32gui
+        hwnd = win32gui.FindWindow(None, window_name)
+
+        left, top, right, bottom = win32gui.GetClientRect(hwnd)
+        w = right - left
+        h = bottom - top
+
+        hwnd_dc = win32gui.GetWindowDC(hwnd)
+        mfc_dc = win32ui.CreateDCFromHandle(hwnd_dc)
+        save_dc = mfc_dc.CreateCompatibleDC()
+        bitmap = win32ui.CreateBitmap()
+        bitmap.CreateCompatibleBitmap(mfc_dc, w, h)
+        save_dc.SelectObject(bitmap)
+
+        # If Special K is running, this number is 3. If not, 1
+        result = windll.user32.PrintWindow(hwnd, save_dc.GetSafeHdc(), 3)
+
+        bmpinfo = bitmap.GetInfo()
+        bmpstr = bitmap.GetBitmapBits(True)
+
+        img = np.frombuffer(bmpstr, dtype=np.uint8).reshape((bmpinfo["bmHeight"], bmpinfo["bmWidth"], 4))
+        img = np.ascontiguousarray(img)[..., :-1]  # make image C_CONTIGUOUS and drop alpha channel
+
+        if not result:  # result should be 1
+            win32gui.DeleteObject(bitmap.GetHandle())
+            save_dc.DeleteDC()
+            mfc_dc.DeleteDC()
+            win32gui.ReleaseDC(hwnd, hwnd_dc)
+            raise RuntimeError(f"Unable to acquire screenshot! Result: {result}")
+
+        cv2.imwrite(CURRENT_SCREEN_IMAGE_PATH, img)
+    except:
+        cv2.imwrite(CURRENT_SCREEN_IMAGE_PATH, cv2.imread(SCREEN_IMAGE_PATH))
 
 def capture_and_censor_screen(detection_areas, censor_mode=''):
     # Pixelate function
@@ -181,6 +224,8 @@ def check_image_similarity(image1_path, image2_path):
     # Read the images in grayscale mode
     image1 = cv2.imread(image1_path, cv2.IMREAD_GRAYSCALE)
     image2 = cv2.imread(image2_path, cv2.IMREAD_GRAYSCALE)
+    (H, W) = image1.shape
+    image2 = cv2.resize(image2, (W, H))
 
     if image1 is None or image2 is None:
         raise ValueError("One of the images could not be read. Please check the file paths.")
@@ -191,7 +236,6 @@ def check_image_similarity(image1_path, image2_path):
     # Compute SSIM between two images
     score, _ = ssim(image1, image2, full=True)
     similarity_percentage = score * 100
-
     return similarity_percentage > IMAGE_SIMILARITY
 
 def censoring_task():
@@ -199,6 +243,7 @@ def censoring_task():
     while True:
         capture_save_screen()
         if (not check_image_similarity(CURRENT_SCREEN_IMAGE_PATH, SCREEN_IMAGE_PATH)):
+            send_clear_censor_block_signal()
             # If images (current and previous) are not similar
             delete_rename_file(CURRENT_SCREEN_IMAGE_PATH, SCREEN_IMAGE_PATH)
             detections = nude_detector.detect(SCREEN_IMAGE_PATH)
